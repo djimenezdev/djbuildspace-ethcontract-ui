b37630144b09b9ecd4a2e499bf4873845ddb3ee1
Daniel Jimenez <daniel@djimenezdev.com>
2022-05-27 18:51:41 -0700

    page data

---
+++ src/data/waveLink.js
@@ -0,0 +1,14 @@
+export const waveLinkInfo = [
+  {
+    title: "Rinkeby Faucet - ",
+    link: "https://rinkebyfaucet.com/",
+  },
+  {
+    title: "Rinkeby: Authenticated Faucet - ",
+    link: "https://faucet.rinkeby.io/",
+  },
+  {
+    title: "Faucets | Chainlink - ",
+    link: "https://faucets.chain.link/",
+  },
+];

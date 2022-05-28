68f74981c46f847538e3dd510b5d5d434cd87f04
Daniel Jimenez <daniel@djimenezdev.com>
2022-05-27 18:49:53 -0700

    project config

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+module.exports = {
+  webpack: {
+    alias: {
+      "@comp": path.resolve(__dirname, "src/components/"),
+      "@img": path.resolve(__dirname, "src/images/"),
+      "@data": path.resolve(__dirname, "src/data/"),
+      "@utils": path.resolve(__dirname, "src/utils/"),
+      "@context": path.resolve(__dirname, "src/context/"),
+    },
+  },
+};

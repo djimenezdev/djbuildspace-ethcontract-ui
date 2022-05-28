a24872115d65af15e0dd714223da49f21b17d1ee
Daniel Jimenez <daniel@djimenezdev.com>
2022-05-27 18:50:36 -0700

    contract config

---
+++ src/utils/variables.js
@@ -0,0 +1,5 @@
+import abi from "@utils/WavePortal.json";
+
+export const contractAddress = "0xF74Bbc845e042e729a3d18475c3c1c2FA00cFb6B";
+export const contractABI = abi.abi;
+export const reCaptchaKey = "6LefrR0gAAAAADK7IHqRXIBna9aKH-b-p3BggGXH";
